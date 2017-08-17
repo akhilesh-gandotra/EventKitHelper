708c6e32c77929db794516cb56b01f0617a43822
Akhilesh90 <akgandotra90@gmail.com>
2017-08-18 00:24:55 +0530

    initial commit

---
+++ EventKitDemo/EventKitHelper.swift
@@ -0,0 +1,175 @@
+//
+//  EventHelper.swift
+//
+//  Created by Akhilesh on 28/07/17.
+//  Copyright © 2017 Akhilesh Gandotra. All rights reserved.
+//
+
+import Foundation
+import EventKit
+import UIKit
+
+struct CalendarEvent {
+    let title: String
+    let startDate: String
+    let endDate: String
+    let description: String?
+}
+enum CalendarResult {
+    case success(CalendarEvent)
+    case error(Error)
+}
+
+class EventKitHelper {
+    
+    //MARK: Variables
+    static var shared: EventKitHelper?
+    let appleEventStore = EKEventStore()
+    private var isPermissionGranted = false  //for checking permission
+    public var calendarName = "My New Calendar"
+  
+    //MARK: For initializing the calendar
+    func generateEvent() {
+        let status = EKEventStore.authorizationStatus(for: EKEntityType.event)
+        switch (status)
+        {  
+        case EKAuthorizationStatus.notDetermined:  // This happens on first-run
+            requestAccessToCalendar()
+            
+        case EKAuthorizationStatus.authorized:
+            self.isPermissionGranted = true
+            
+        case EKAuthorizationStatus.restricted, EKAuthorizationStatus.denied:
+            // We need to help them give us permission
+            self.isPermissionGranted = false
+            print("User has to change settings...goto settings to view access")
+        }
+    }
+    
+    init() {
+        generateEvent()
+    }
+    
+    
+    
+    //MARK: For fetching events from a particular calendar
+    public func getEventsFromCalenderWith(identifier: String, startingTimeInterval: TimeInterval, endingTimeInterval: TimeInterval) -> [EKEvent] {
+        let calendars = appleEventStore.calendars(for: .event)
+        for calendar in calendars {
+            if calendar.title == identifier {
+            
+                let startingTime = Date(timeIntervalSinceNow: startingTimeInterval)
+                let endingTime = Date(timeIntervalSinceNow: endingTimeInterval)
+                
+                let predicate = appleEventStore.predicateForEvents(withStart: startingTime, end: endingTime, calendars: [calendar])
+                
+                
+                let events = appleEventStore.events(matching: predicate)
+                var eventArray = [EKEvent]()
+                for event in events {
+                   eventArray.append(event)
+                }
+                return eventArray
+            }
+ 
+        }
+        return [EKEvent]()
+    }
+    
+    //MARK: For checking permission to access calendar
+   private func requestAccessToCalendar() {
+        appleEventStore.requestAccess(to: .event, completion: { (granted, error) in
+            if (granted) && (error == nil) {
+                    print("User has access to calendar")
+                    self.isPermissionGranted = true
+                    self.addCalendar(calendarName: self.calendarName)
+                
+            } else {
+                self.isPermissionGranted = false
+            }
+        })
+    }
+    
+    
+    public func addAppleEvents(calenderEvent: CalendarEvent, callback: ((CalendarResult) -> Void)) {
+        addCalendar(calendarName: calendarName)
+        
+        let event:EKEvent = EKEvent(eventStore: appleEventStore)
+        event.title = calenderEvent.title
+        event.startDate = convertDateFormatter(string: calenderEvent.startDate)
+        event.endDate = convertDateFormatter(string: calenderEvent.endDate)
+        event.notes = calenderEvent.description
+        let calendars = appleEventStore.calendars(for: .event)
+        for abc in calendars {
+            if abc.title == calendarName {
+                event.calendar = abc
+            }
+        }
+
+        event.addAlarm(EKAlarm(absoluteDate: convertDateFormatter(string: calenderEvent.startDate)))
+        
+        do {
+            try appleEventStore.save(event, span: .thisEvent)
+            print("Event Added")
+            callback(.success(calenderEvent))
+        } catch let error as NSError {
+            print(error.description)
+            if self.isPermissionGranted {
+            callback(.error(error))
+            } else {
+                let errorTemp = NSError(domain:EKErrorDomain, code: 20, userInfo: ["message":"Permission not granted for calendar"])
+                callback(.error(errorTemp))
+            }
+            
+        }
+    }
+    
+    //MARK: For Adding a new calendar into the calendars
+   private func addCalendar(calendarName: String) {
+    appleEventStore.requestAccess(to: .event, completion: { (granted, error) in
+            if (granted) && (error == nil) {
+             self.isPermissionGranted = true
+            } else {
+               self.isPermissionGranted = false
+        }
+        })
+        if !self.isPermissionGranted {
+            return
+        }
+        
+        let calendars = appleEventStore.calendars(for: .event)
+        for abc in calendars {
+            if abc.title == calendarName {
+             return
+            }
+        }
+        let eventStore = EKEventStore()
+        let newCalendar = EKCalendar(for: .event, eventStore: eventStore)
+        newCalendar.title = calendarName
+        let sourcesInEventStore = eventStore.sources
+        newCalendar.source = sourcesInEventStore.filter{
+            (source: EKSource) -> Bool in
+            source.sourceType == EKSourceType.local
+            }.first!
+        do {
+            try eventStore.saveCalendar(newCalendar, commit: true)
+            UserDefaults.standard.set(newCalendar.calendarIdentifier, forKey: "EventTrackerPrimaryCalendar")
+            print("Calendar is set now")
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    //MARK: String to date convertor
+    func convertDateFormatter(string: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"   //this your string date format
+        let date = dateFormatter.date(from: string)
+
+        guard let convertedDate = date else {
+            fatalError("date format is wrong")
+        }
+        return convertedDate
+    }
+}
--- EventKitDemo/ViewController.swift
+++ EventKitDemo/ViewController.swift
@@ -5,21 +5,68 @@
 //  Created by soc-macmini-45 on 17/08/17.
 //  Copyright © 2017 soc-macmini-45. All rights reserved.
 //
-
 import UIKit
+import  EventKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var allEvents = [EKEvent]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        EventKitHelper.shared = EventKitHelper() // for initializing calendar and asking permissions (required)
+        EventKitHelper.shared?.calendarName = "New Calendar" //if u want to give a name to the calendar else (My new calendar by default)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func fetchAllEvvars(_ sender: Any) {
+        allEvents =  (EventKitHelper.shared?.getEventsFromCalenderWith(identifier: (EventKitHelper.shared?.calendarName)!, startingTimeInterval: -30*24*3600, endingTimeInterval: +30*24*3600))!
+        tableView.reloadData()
     }
-
-
+    
+    
+    @IBAction func eventACtion(_ sender: Any) {
+    //   -------This is how you add event in calendar using the Event helper---------
+        
+        
+        let event = CalendarEvent(title: "lets see", startDate: "2017-08-12T18:10:52.000Z", endDate: "2017-08-13T15:10:52.000Z", description: "Notes added")
+        
+        EventKitHelper.shared?.addAppleEvents(calenderEvent: event, callback: { (result) in
+            switch result {
+            case .success(let event):
+                print(event.title)
+                NSLog("event.title\(event.title)")
+            case .error(let error):
+                print(error)
+            }
+        })
+    }
+    
+    @IBAction func clearAction(_ sender: UIButton) {
+        allEvents.removeAll()
+        tableView.reloadData()
+    }
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allEvents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! Cell
+        cell.nameLabel.text = allEvents[indexPath.row].title
+        cell.date.text = "\(allEvents[indexPath.row].startDate)"
+        return cell
+    }
+}
+
+class Cell: UITableViewCell {
+    
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+}
+
 
