b5330f9258b23394b02321293b71528a06e84f03
Akhilesh Gandotra <akhilesh.gandotra@click-labs.com>
2017-08-17 17:40:40 +0530

    Initial Commit

---
+++ EventKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventKitDemo
+//
+//  Created by soc-macmini-45 on 17/08/17.
+//  Copyright © 2017 soc-macmini-45. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
